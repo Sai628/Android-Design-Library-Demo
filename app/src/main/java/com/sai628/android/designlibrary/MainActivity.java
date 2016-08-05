9b57a6367c164442dd2e7cbf724103e2c548d378
Sai628 <sai100628@gmail.com>
2016-08-05 17:12:24 +0800

    first commit

---
+++ app/src/main/java/com/sai628/android/designlibrary/MainActivity.java
@@ -0,0 +1,16 @@
+package com.sai628.android.designlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
